030f41d13caa5d78a737975e4d61b3ecfe67c761
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2022-12-04 21:31:04 -0800

    added py

---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print('Hello, World!')
+print('gonna use python to do data analysis or test code in simulation or smth like that, basically any automated task that isn\'t uploaded to microcontroller')
