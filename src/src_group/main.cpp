5ff7b38a0c0137368348a23cf59a9da6f8f99d55
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2022-12-05 21:32:33 -0800

    separate drehmflight

---
+++ src/src_group/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "src_group/dRehmFlight_headerpins.h"
+#include "src_group/dRehmFlight_functions.h"
+#include "src_group/dRehmFlight_functions_setup_loop.h"
+
+
+//normally run dRhemFlightSetup and dRehmFlightLoop, but for testing run individual functions
+
+void setup() {
+    //dRehmFlightSetup();
+}
+
+void loop() {
+    //dRehmFlightLoop();
+}
