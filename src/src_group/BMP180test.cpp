d111743fba6e822ab412b572047418c40546a8c8
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2022-12-11 12:49:23 -0800

    bmp altitude and filter working

---
+++ src/src_group/BMP180test.cpp
@@ -0,0 +1,34 @@
+// works as of 12/11/22
+
+#include <Arduino.h>
+#include "src_group/dRehmFlight.h"
+#include "src_group/dataLog.h"
+#include "src_group/ToF/VL53L1X.h"
+#include "BMP180.h"
+// add other libraries when they come in
+
+void setup()
+{
+    Serial.begin(500000); // USB serial
+    delay(500);
+    setupBlink(3, 160, 70); // numBlinks, upTime (ms), downTime (ms)
+    BMP180setup();
+}
+
+void loop()
+{
+    prev_time = current_time;
+    current_time = micros();
+    dt = (current_time - prev_time) / 1000000.0;
+    loopBlink();    // Indicate we are in main loop with short blink every 1.5 seconds
+    loopRate(2000); // Do not exceed 2000Hz, all filter parameters tuned to 2000Hz by default
+    BMP180loop();
+    // Serial.print(pressureMeasured);
+    // Serial.print(" ");
+    // Serial.print(pressure);
+    // Serial.print(" ");
+    Serial.print(altitudeMeasured);
+    Serial.print(" ");
+    Serial.print(altitude);
+    Serial.println();
+}
