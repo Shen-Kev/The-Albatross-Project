591d11669da88ee82288e7483384757af4578d6b
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2022-12-10 23:27:52 -0800

    debugging filepaths

--- src/src_group/main.cpp
+++ src/src_group/main.cpp
@@ -1,9 +1,9 @@
 //MAIN CODE, waiting to run some functions oh yeah
 
 #include <Arduino.h>
-
 #include "src_group/dRehmFlight.h"
-#include "dataLog.h"
+#include "src_group/dataLog.h"
+#include "src_group/VL53L1X.h"
 //add other libraries when they come in
 
 void setup()
