4f411fe92f237973988208ed11ea94ccca49fb95
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2022-12-11 12:11:07 -0800

    added low pass to distance

--- src/src_group/ToF/VL53L1X.h
+++ src/src_group/ToF/VL53L1X.h
@@ -4,38 +4,25 @@
 #define XSHUT_PIN 36
 void ToFsetup();
 void ToFloop();
+int16_t distance;
+float distance_LP_param = 0.5;
+float distancePrev;
+float distance_LP;
 
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
 
 void VL53L1Xsetup() {
-  Serial.begin(115200);
-  while (!Serial) delay(10);
-
-  Serial.println(F("Adafruit VL53L1X sensor demo"));
-
   Wire.begin();
   if (! vl53.begin(0x29, &Wire)) {
-    Serial.print(F("Error on init of VL sensor: "));
-    Serial.println(vl53.vl_status);
     while (1)       delay(10);
   }
-  Serial.println(F("VL53L1X sensor OK!"));
-
-  Serial.print(F("Sensor ID: 0x"));
-  Serial.println(vl53.sensorID(), HEX);
 
   if (! vl53.startRanging()) {
-    Serial.print(F("Couldn't start ranging: "));
-    Serial.println(vl53.vl_status);
     while (1)       delay(10);
   }
-  Serial.println(F("Ranging started"));
 
   // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500ms!
   vl53.setTimingBudget(100);
-  Serial.print(F("Timing budget (ms): "));
-  Serial.println(vl53.getTimingBudget());
-
   /*
   vl.VL53L1X_SetDistanceThreshold(100, 300, 3, 1);
   vl.VL53L1X_SetInterruptPolarity(0);
@@ -43,22 +30,15 @@ void VL53L1Xsetup() {
 }
 
 void VL35L1Xloop() {
-  int16_t distance;
-
   if (vl53.dataReady()) {
     // new measurement for the taking!
     distance = vl53.distance();
     if (distance == -1) {
-      // something went wrong!
-      Serial.print(F("Couldn't get distance: "));
-      Serial.println(vl53.vl_status);
       return;
     }
-    Serial.print(F("Distance: "));
-    Serial.print(distance);
-    Serial.println(" mm");
-
     // data is read out, time for another reading!
+    distance_LP = (1.0 - distance_LP_param) * distancePrev + distance_LP_param * distance;
+    distancePrev = distance_LP;
     vl53.clearInterrupt();
   }
 }
---
+++ src/src_group/ToFtest.cpp
@@ -0,0 +1,32 @@
+//MAIN CODE, waiting to run some functions oh yeah
+//works as of 12/10/22
+
+#include <Arduino.h>
+#include "src_group/dRehmFlight.h"
+#include "src_group/dataLog.h"
+#include "src_group/ToF/VL53L1X.h"
+//add other libraries when they come in
+
+void setup()
+{
+    Serial.begin(500000); // USB serial
+    delay(500);
+    setupBlink(3, 160, 70); // numBlinks, upTime (ms), downTime (ms)
+    VL53L1Xsetup();
+}
+
+void loop()
+{
+    prev_time = current_time;
+    current_time = micros();
+    dt = (current_time - prev_time) / 1000000.0;
+    loopBlink(); // Indicate we are in main loop with short blink every 1.5 seconds
+    loopRate(2000); // Do not exceed 2000Hz, all filter parameters tuned to 2000Hz by default
+    VL35L1Xloop();
+    Serial.print(" Distance: ");
+    Serial.print(distance);
+    Serial.print(" DistanceLP: ");
+    Serial.print(distance_LP);
+    Serial.println(" mm");
+    //will need to make some code for when distance returns -1, that the kalman filter or whatever completely ignores it and just uses IMU and barometer
+}
