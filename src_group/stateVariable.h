dd5f1d678b2d8550a33be4a465e56229757e3b0d
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2022-12-18 13:49:17 -0800

    not sure what happened to src group

---
+++ src_group/stateVariable.h
@@ -0,0 +1,38 @@
+#include "src_group/dRehmFlight.h"
+#include "src_group/ToF/VL53L1X.h"
+#include "BMP180.h"
+
+//State variables:
+/*
+attitude  (IMU orientation and compass heading)
+rotation rate of change (IMU gyroscope)
+lateral movement (IMU acceleration)
+altitude (IMU, ToF, baro)
+airspeed
+*/
+//code also needs to calibrate the sensors, and point them in the right direction using servos
+
+//FINAL STATE VECTOR VALUES:
+float roll_angle;
+float roll_rate;
+float pitch_angle;
+float pitch_rate;
+float yaw_angle; //relative to magnetic north
+float yaw_rate;
+float horizontal_acceleration;  //always horizontal (parallel to ground), even when banking
+float altitude; //distance to ground
+float rate_of_climb;
+float airspeed;
+
+//altitude related variables, like prev, and stuff, and angle of servo motor needed
+//maybe kalman filter... idk 
+
+
+void calibrateAltitude() {
+
+}
+
+void calculateAltitude() {
+
+}
+
