55d765f6508a4a7e716d9a065ae970e73434a081
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2023-02-28 21:37:45 -0800

    YES PITCH AND ROLL ERROR graph work

--- flight data editor/CSV_plot_DS_cycle.py
+++ flight data editor/CSV_plot_DS_cycle.py
@@ -91,7 +91,14 @@ airspeed_adjusted_column = df.iloc[:, airspeed_adjusted]
 s1_command_scaled_column = df.iloc[:, s1_command_scaled]
 forwardsAcceleration_column = df.iloc[:, forwardsAcceleration]
 
-#insert Nan values if there is a gap between time values in the data greater than 1 second
+# calculat roll and pitch error columns
+roll_error_column = np.full(len(time), np.nan)
+pitch_error_column = np.full(len(time), np.nan)
+for i in range(len(time)):
+    roll_error_column[i] = roll_IMU_column[i] - roll_des_column[i]
+    pitch_error_column[i] = pitch_IMU_column[i] - pitch_des_column[i]
+
+# insert Nan values if there is a gap between time values in the data greater than 1 second
 for i in range(len(time)):
     if i == 0:
         continue
@@ -113,12 +120,42 @@ title = "Dynamic Soaring Flight Data"
 fig.suptitle(title, y=0.95)
 
 
-# plot the charts
-roll.plot(time, roll_IMU_column, label="Roll IMU", color='red')
+# plot the charts.
+
+# for roll, if the roll error at this time is greater than 10 degrees, then plot the roll error in red, otherwise plot it in green
+roll_column_high_error = np.full(len(time), np.nan)
+roll_column_low_error = np.full(len(time), np.nan)
+
+for i in range(len(time)):
+    if abs(roll_error_column[i]) > 10:
+        roll_column_high_error[i] = roll_IMU_column[i]
+    else:
+        roll_column_low_error[i] = roll_IMU_column[i]
+
+roll.plot(time, roll_column_high_error, label="Abs. Error > 10 deg", color='red')
+roll.plot(time, roll_column_low_error, label="Abs. Error < 10 deg", color='green')
+#roll.plot(time, roll_IMU_column, label="Roll IMU", color='blue')
+roll.plot(time, roll_des_column, label="Roll Desired", color='orange')
+#roll.plot(time, roll_error_column, label="Roll Error", color='purple')
+
 roll.set_ylabel("deg")
 roll.legend()
 
-pitch.plot(time, pitch_IMU_column, label="Pitch IMU", color='green')
+#same for pitch
+pitch_column_high_error = np.full(len(time), np.nan)
+pitch_column_low_error = np.full(len(time), np.nan)
+
+for i in range(len(time)):
+    if abs(pitch_error_column[i]) > 10:
+        pitch_column_high_error[i] = pitch_IMU_column[i]
+    else:
+        pitch_column_low_error[i] = pitch_IMU_column[i]
+
+pitch.plot(time, pitch_column_high_error, label="Abs. Error > 10 deg", color='red')
+pitch.plot(time, pitch_column_low_error, label="Abs. Error < 10 deg", color='green')
+#pitch.plot(time, pitch_IMU_column, label="Pitch IMU", color='blue')
+pitch.plot(time, pitch_des_column, label="Pitch Desired", color='orange')
+#pitch.plot(time, pitch_error_column, label="Pitch Error", color='purple')
 
 pitch.set_ylabel("deg")
 pitch.legend()
@@ -139,62 +176,4 @@ plt.subplots_adjust(top=0.9)
 plt.show()
 
 
-# calculate the error of the PID loops. Each value is paired with the value in the same row of the other column
-roll_error = roll_IMU_column - roll_des_column
-pitch_error = pitch_IMU_column - pitch_des_column
 
-
-#remove outliers using IQR method where an outlier is defined as a value that is more than 1.5 times the IQR away from the median
-#find the median
-median_roll_error = np.median(roll_error)
-#find the first and third quartiles
-q1_roll_error, q3_roll_error = np.percentile(roll_error, [25, 75])
-#find the interquartile range
-iqr_roll_error = q3_roll_error - q1_roll_error
-#find the upper and lower bounds
-lower_bound_roll_error = q1_roll_error - (1.5 * iqr_roll_error)
-upper_bound_roll_error = q3_roll_error + (1.5 * iqr_roll_error)
-#remove outliers
-roll_error = roll_error[roll_error.between(lower_bound_roll_error, upper_bound_roll_error, inclusive=True)]
-
-#remove outliers using IQR method where an outlier is defined as a value that is more than 1.5 times the IQR away from the median
-#find the median
-median_pitch_error = np.median(pitch_error)
-#find the first and third quartiles
-q1_pitch_error, q3_pitch_error = np.percentile(pitch_error, [25, 75])
-#find the interquartile range
-iqr_pitch_error = q3_pitch_error - q1_pitch_error
-#find the upper and lower bounds
-lower_bound_pitch_error = q1_pitch_error - (1.5 * iqr_pitch_error)
-upper_bound_pitch_error = q3_pitch_error + (1.5 * iqr_pitch_error)
-#remove outliers
-pitch_error = pitch_error[pitch_error.between(lower_bound_pitch_error, upper_bound_pitch_error, inclusive=True)]
-
-
-
-
-#plot when the pitch error is within 10 deg of the setpoints. make the plot so that each datapoint is represented by a bar that sticks up or down from 0 depending on the error. if it is within 10, make the bar green, and if its outside, make the bar red.
-deg_tolerance = 10
-plt.bar(pitch_error.index, pitch_error, color=pitch_error.apply(lambda x: 'g' if x < deg_tolerance and x > -deg_tolerance else 'r'))
-plt.title("Pitch Error")
-plt.xlabel("Time (s)")
-plt.ylabel("Error (degrees)")
-plt.show()
-
-#calculate what percent of the time the pitch error is within 5 deg of the setpoint
-pitch_error_within_tolerance = pitch_error[(pitch_error < deg_tolerance) & (pitch_error > -deg_tolerance)]
-pitch_error_within_tolerance = len(pitch_error_within_tolerance)/len(pitch_error)
-print("Percent of time pitch error is within 10 deg of setpoint: " + str(pitch_error_within_tolerance))
-
-#same for roll
-deg_tolerance = 10
-plt.bar(roll_error.index, roll_error, color=roll_error.apply(lambda x: 'g' if x < deg_tolerance and x > -deg_tolerance else 'r'))
-plt.title("Roll Error")
-plt.xlabel("Time (s)")
-plt.ylabel("Error (degrees)")
-plt.show()
-
-#calculate what percent of the time the roll error is within 5 deg of the setpoint
-roll_error_within_tolerance = roll_error[(roll_error < deg_tolerance) & (roll_error > -deg_tolerance)]
-roll_error_within_tolerance = len(roll_error_within_tolerance)/len(roll_error)
-print("Percent of time roll error is within 10 deg of setpoint: " + str(roll_error_within_tolerance))
